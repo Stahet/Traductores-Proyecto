1b20f8731c18df85dc0203ffca85eb1a9268462e
manuggz <manu.gz933@gmail.com>
2015-02-06 00:29:23 -0430

    arreglado bug chino xD falta precedencia

--- Setlan/Entrega2(Parser)/parser.py
+++ Setlan/Entrega2(Parser)/parser.py
@@ -9,31 +9,34 @@ import ply.yacc as yacc
 import ply.lex as lexi
 
 import expresiones
+
 from AST import *
 
 tokens = expresiones.tokens
 
+
 def p_program(p):
-    'PROGRAM : PROGRAM STATEMENT'
-    p[0] = Program(p[1],p[2])        
+    'program : PROGRAM statement'
+    p[0] = Program(p[1],p[2])
+    
 
 def p_statement_assing(p):
-    '''STATEMENT : IDENTIFIER ASSIGN EXPRESSION'''
+    '''statement : IDENTIFIER ASSIGN expression'''
     p[0] = Assign(p[1],p[3])
     
 def p_statement_block(p):
-    '''STATEMENT : LCURLY DECLARE STATEMENT_LIST RCURLY '''
+    '''statement : LCURLY declare statement_list RCURLY '''
     p[0] = Block(p[2],p[3])
 
 def p_statement_function(p):
-    '''STATEMENT : SCAN IDENTIFIER 
-                 | PRINT   COMMA_LIST
-                 | PRINTLN COMMA_LIST'''
+    '''statement : SCAN IDENTIFIER 
+                 | PRINT   comma_list
+                 | PRINTLN comma_list'''
     
     p[0] = FunctionBuiltIn(p[1],p[2])
 
 def p_expression_dato(p):
-    '''EXPRESSION : INTEGER
+    '''expression : INTEGER
             | FALSE
             | TRUE
             | STRING
@@ -42,94 +45,94 @@ def p_expression_dato(p):
     p[0] = p[1]
 
 def p_expression_parent(p):
-    ''' EXPRESSION : LPARENT EXPRESSION RPARENT'''
+    ''' expression : LPARENT expression RPARENT'''
     p[0] = Parenthesis(p[2])
 
 def p_expression_curly(p):
-    ''' EXPRESSION : LCURLY EXPRESSION RCURLY'''
+    ''' expression : LCURLY expression RCURLY'''
     p[0] = Block(p[2])
 
 def p_expression_op_bin(p):
-    '''EXPRESSION:EXPRESSION '+' EXPRESSION            
-                 | EXPRESSION '-' EXPRESSION            
-                 | EXPRESSION '*' EXPRESSION            
-                 | EXPRESSION '/' EXPRESSION            
-                 | EXPRESSION '%' EXPRESSION            
-                 | EXPRESSION '==' EXPRESSION
-                 | EXPRESSION '/=' EXPRESSION
-                 | EXPRESSION '<' EXPRESSION
-                 | EXPRESSION '<=' EXPRESSION
-                 | EXPRESSION '>' EXPRESSION
-                 | EXPRESSION '>=' EXPRESSION
-                 | EXPRESSION '@' EXPRESSION
-                 | EXPRESSION '++' EXPRESSION
-                 | EXPRESSION '\' EXPRESSION
-                 | EXPRESSION '><' EXPRESSION
-                 | EXPRESSION '<+>' EXPRESSION
-                 | EXPRESSION '<->' EXPRESSION
-                 | EXPRESSION '<*>' EXPRESSION
-                 | EXPRESSION '</>' EXPRESSION
-                 | EXPRESSION '<%>' EXPRESSION
+    '''expression : expression PLUS expression            
+                 | expression MINUS expression            
+                 | expression TIMES expression            
+                 | expression INTDIVISION expression            
+                 | expression RESTDIVISION expression            
+                 | expression '==' expression
+                 | expression '/=' expression
+                 | expression '<' expression
+                 | expression '<=' expression
+                 | expression '>' expression
+                 | expression '>=' expression
+                 | expression '@' expression
+                 | expression '++' expression
+                 | expression '\' expression
+                 | expression '><' expression
+                 | expression '<+>' expression
+                 | expression '<->' expression
+                 | expression '<*>' expression
+                 | expression '</>' expression
+                 | expression '<%>' expression
     '''
     
     p[0] = BinaryOP(p[1],p[2],p[3])
     
 def p_expression_op_unary(p):
-    '''EXPRESSION : '-' EXPRESSION
-                  | >? EXPRESSION
-                  | <? EXPRESSION
-                  | $? EXPRESSION'''
+    '''expression : '-' expression
+                  | >? expression
+                  | <? expression
+                  | $? expression'''
     p[0] = UnaryOP(p[1],p[2])
     
 def p_comma_list_expression(p):
-    '''COMMA_LIST : EXPRESSION'''
+    '''comma_list : expression'''
     p[0] = p[1]
     
 def p_comma_list_expression_comma_continue(p):
-    '''COMMA_LIST : EXPRESSION , COMMA_LIST'''
+    '''comma_list : expression , comma_list'''
     p[0] = SeparetedTerms(p[1],p[2],p[3])
     
 def p_statement_list(p):
-    '''STATEMENT_LIST : STATEMENT'''
+    '''statement_list : statement'''
     p[0] = p[1]
     
 def p_statement_list_continue(p):
-    '''STATEMENT_LIST : STATEMENT ; STATEMENT_LIST'''
+    '''statement_list : statement ; statement_list'''
     p[0] = SeparetedTerms(p[1],p[2],p[3])
 
 def p_declare_vars(p):
-    '''DECLARE : 'using' DECLARE_LIST 'in' '''
+    '''declare : USING declare_list IN '''
     p[0] = p[2]
 
 def p_declare_empty(p):
-    '''DECLARE :EMPTY'''
+    '''declare :empty'''
     pass
 
 def p_declare_list_type(p):
-    '''DECLARE_LIST : TYPE 'identifier' DECLARE_LIST_CONTINUE'''
+    '''declare_list : type IDENTIFIER declare_list_continue'''
     p[0] = DeclareList(p[1],p[2],p[3])
 
 def p_declare_list_(p):
-    '''DECLARE_LIST : DECLARE_LIST ; DECLARE_LIST'''
+    '''declare_list : declare_list ; declare_list'''
     p[1].declared_list = p[1].declared_list + p[3].declared_list
     p[0] = p[1]
 
 def p_declare_list_continue_end(p):
-    '''DECLARE_LIST_CONTINUE: ;'''
+    '''declare_list_continue : ;'''
     p[0] = []
 
 def p_declare_list_continue_on(p):
-    '''DECLARE_LIST_CONTINUE: , IDENTIFIER DECLARE_LIST_CONTINUE'''
+    '''declare_list_continue : , IDENTIFIER declare_list_continue'''
     p[0] = [p[2]] + [p[3]]
 
 def p_direction_all(p):
-    ''''DIRECTION : min
+    ''''direction : min
                   | max
     '''
     p[0] = p[1]
 
 def p_type_data(p):
-    '''TYPE: 'int' 
+    '''type: 'int' 
            | 'bool'
            | 'set'
     '''
